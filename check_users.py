8765c1e59467d131f497dd2f41d3b9a251acd094
Broken Developer <brokendev2007@gmail.com>
2026-03-05 22:21:25 +0530

    Removed heavy folders

--- check_users.py
+++
@@ -1,9 +0,0 @@
-from backend.database import SessionLocal
-from backend.models import User
-
-db = SessionLocal()
-users = db.query(User).all()
-print("Total users:", len(users))
-for u in users:
-    print(u.id, u.name)
-db.close()
